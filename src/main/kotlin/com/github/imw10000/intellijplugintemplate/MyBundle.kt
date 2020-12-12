a2f29244e3a1f5b764e2c76cb6783a62cba62425
GitHub Action <action@github.com>
2020-12-12 00:26:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/imw10000/intellijplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/imw10000/intellijplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.imw10000.intellijplugintemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/imw10000/intellijplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.imw10000.intellijplugintemplate.services
+
+import com.github.imw10000.intellijplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
